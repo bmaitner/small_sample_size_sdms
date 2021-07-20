3714eac4cf646d542822c4800f1b02d1520e6330
Brian <bmaitner@gmail.com>
2021-07-20 16:54:11 -0400

    removed KLIEP, as the underlying funciton in densratio has a serious bug

--- R/fit_density_ratio.R
+++ R/fit_density_ratio.R
@@ -2,7 +2,7 @@
 #' @param background Optional. Dataframe of covariates at background points
 #' @param method Optional. If supplied, both presence and background density estimation will use this method.
 #' @param ... Additional parameters passed to internal functions.
-#' @details Current methods include: "ulsif", "rulsif","kliep"
+#' @details Current methods include: "ulsif", "rulsif"
 #' @export
 #' @return List of class "dr_model" containing model objects and metadata needed for projecting the fitted models.
 fit_density_ratio <- function(presence = NULL,
@@ -10,6 +10,15 @@ fit_density_ratio <- function(presence = NULL,
                               method = NULL,
                               ...){
   #Check data and method
+  
+  #for now do this manually, but once function skeleton is working do this by looking up available internals
+  current_modules <- c("ulsif","rulsif")
+  
+  if(!method %in% current_modules) {
+    stop(paste("Method not implemented. Please select one of: ",
+               paste(current_modules,collapse = ", "),".",sep =  ))
+  }
+  
   
   #Fit the ratio
   dr <- do.call(what = paste('dr_', method, sep = ""),
