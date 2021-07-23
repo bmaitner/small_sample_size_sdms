081a151e6a7b115ede07767907b375915fff4e48
Brian <bmaitner@gmail.com>
2021-07-23 15:24:04 -0400

    added stratification scripts

--- R/sdm_threshold.R
+++ R/sdm_threshold.R
@@ -1,9 +1,4 @@
 
-
-###########################
-
-#so in order to do the thresholding, I need to 
-
 #' @param prediction_raster Raster containing continuous predictions of "suitability" to be thresholded
 #' @param occurrence_sp A spatialpoints object containing presence location. in the projection of the prediciton raster
 #' @param quantile Numeric between 0 and 1. Quantile to use for thresholding (defaults to 0.05).  Set to 0 for minimum training presence.
@@ -11,35 +6,28 @@
 #' @keywords internal
 #' @importFrom stats quantile
 #' @author Cecina Babich Morrow (modified by Brian Maitner)
-sdm_threshold <- function(prediction_raster, occurrence_sp, quantile = 0.05, return_binary = TRUE){
-  
-  predictions_at_occurrences <- extract(y = occurrence_sp,x = prediction_raster)
-  
+sdm_threshold <- function(prediction_raster,
+                          occurrence_sp,
+                          quantile = 0.05,return_binary = TRUE){
+
+  predictions_at_occurrences <- extract(y = occurrence_sp,
+                                        x = prediction_raster)
+
   threshold <- stats::quantile(x = predictions_at_occurrences,
                                probs = quantile)
-  
+
   if(return_binary){
 
     prediction_raster <- prediction_raster > threshold
     prediction_raster[prediction_raster == 0] <- NA
-    
+
   }else{
 
     prediction_raster[which(getValues(prediction_raster) < threshold)] <- NA
 
   }
-  
+
   return(prediction_raster)
-  
-  
-  
+
 }
-
-
-
-
-
-
-
-
 
