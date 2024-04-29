434095fc5dbb94e42be533c125785961905e6058
Brian <bmaitner@gmail.com>
2024-04-29 19:34:17 -0400

    starting to write ensemble evaluation code

---
+++ 5_ensemble.R
@@ -0,0 +1,24 @@
+# The goal of this script is to evaluate the performance of an ensemble of models for small sample size species (which will be compared to the model bakeoff and rarified bakeoff)
+
+library(disdat)
+library(np)
+library(AUC)
+library(rvinecopulib)
+library(pROC)
+library(lemon)
+library(pbsdm)
+library(kernlab)
+library(tidyverse)
+library(sf)
+library(DescTools)
+library(foreach)
+library(doParallel)
+source("R/evaluate_disdat.R")
+
+
+# Select models to use in ensemble
+
+  model_vector <- c("maxnet","rulsif","kde/kde")
+
+# set threshold 
+
